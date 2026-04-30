8aa165eb3be6c058e9e8043fdf7fe4dd95355d8e
Julien Delphine <julien.delphine@epitech.eu>
2026-04-30 11:35:34 +0200

    feat(config): update config to fit next 16

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: [
-      "typeorm",
-      "better-sqlite3",
-      "reflect-metadata",
-    ],
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.optimization.minimizer = config.optimization.minimizer.map(
-        (plugin: any) => {
-          if (plugin.constructor.name === "TerserPlugin") {
-            plugin.options.minimizer.options.keep_classnames = true;
-            plugin.options.minimizer.options.keep_fnames = true;
-          }
-          return plugin;
-        },
-      );
-    }
-    return config;
-  },
+  serverExternalPackages: ["typeorm", "better-sqlite3", "reflect-metadata"],
+  turbopack: {},
 };
 
 export default nextConfig;
