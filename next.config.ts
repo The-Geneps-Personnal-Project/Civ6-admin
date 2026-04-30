160fe03e8094b0e0bea22e696d62ccbcaee0a1bc
Julien Delphine <julien.delphine@epitech.eu>
2026-04-30 11:18:20 +0200

    feat(config): update next config for production

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: [
+      "typeorm",
+      "better-sqlite3",
+      "reflect-metadata",
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.optimization.minimizer = config.optimization.minimizer.map(
+        (plugin: any) => {
+          if (plugin.constructor.name === "TerserPlugin") {
+            plugin.options.minimizer.options.keep_classnames = true;
+            plugin.options.minimizer.options.keep_fnames = true;
+          }
+          return plugin;
+        },
+      );
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
